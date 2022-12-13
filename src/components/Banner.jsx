5908ee099a4022bdde93688168566d7ad0c25c11
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 12:25:05 +0000

    Banner importing useState

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 const Banner = () => {
+    const [text, setText] = useState('');
+
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>Hi I'm Patrick - <span className="wrap">This text will be changed</span></h1>
+                        <h1>{`Hi I'm Patrick - `}<span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <button>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
