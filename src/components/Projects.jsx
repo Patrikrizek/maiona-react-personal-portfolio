d20974aebd2c80c652ae5a83df9b0733a569f0d6
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 15:36:21 +0000

    Projects uploading array of projects with images, not using State

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,42 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
 
 const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+    ];
+
     return (
         <section className="project" id="project">
             <Container>
