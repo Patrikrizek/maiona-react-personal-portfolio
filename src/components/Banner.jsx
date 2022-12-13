3379874b8c67c0371afb18612c57dc41134c19bf
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 12:21:46 +0000

    Banner Bootstrap icon import

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 const Banner = () => {
@@ -10,7 +11,7 @@ const Banner = () => {
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>Hi I'm Patrick - <span className="wrap">This text will be changed</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <button>Let's connect</button>
+                        <button>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
 
                     <Col xs={12} md={6} xl={5}>
