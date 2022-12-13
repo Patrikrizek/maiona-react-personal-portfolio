8b8a0fc7833accea25cd486a5cbd37f778316320
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 14:56:22 +0000

    Skills importing carousel

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,27 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
     return (
         <section className="skill" id="skills">
             <div className="container">
@@ -6,6 +29,12 @@ const Skills = () => {
                     <div className="col-12">
                         <div className="skill-bx wow zoomIn">
                             <h2>Skills</h2>
+                            <Carousel responsive={responsive}>
+                                <div>Item 1</div>
+                                <div>Item 2</div>
+                                <div>Item 3</div>
+                                <div>Item 4</div>
+                            </Carousel>
                         </div>
                     </div>
                 </div>
