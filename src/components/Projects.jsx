0facb9e26621318ddf75fec94caea53702fe4377
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 15:14:08 +0000

    Projects Bootstrap Tab Layout import

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,4 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 
 const Projects = () => {
     return (
@@ -10,6 +8,28 @@ const Projects = () => {
                     <Col size={12}>
                         <h2>Projects</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                            <Row>
+                                <Col sm={3}>
+                                    <Nav variant="pills" className="flex-column">
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                        </Nav.Item>
+                                    </Nav>
+                                </Col>
+                                <Col sm={9}>
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="first">
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="second">
+                                        </Tab.Pane>
+                                    </Tab.Content>
+                                </Col>
+                            </Row>
+                        </Tab.Container>
                     </Col>
                 </Row>
             </Container>
