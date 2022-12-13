6c7fb3c8c4d5aa36451d4e758ef21d6bc2293db2
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 12:01:25 +0000

    NavBar importin, useEffect, onScroll function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -9,13 +9,28 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 
 const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
 
   const onUpdateActiveLink = value => {
     setActiveLink(value);
   }
 
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container fluid>
         <Navbar.Brand href="#">
           <img src={logo} alt="logo" />
