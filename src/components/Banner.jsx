374ae1ad714727ce7231895ed5a00c123f4d75d2
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-13 12:42:23 +0000

    Banner tick if statements on text delete and type

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,6 +7,7 @@ const Banner = () => {
     const [loopNum, setLoopNum] = useState(1);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [index, setIndex] = useState(1);
     const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
     const period = 2000;
@@ -17,6 +18,23 @@ const Banner = () => {
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
         setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
     }
 
     return (
