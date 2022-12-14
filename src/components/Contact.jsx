347e67fe44e494558ebf98c5f750e0db00cb939a
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 12:53:36 +0000

    Contact Bootstrap import and content upload

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,42 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 const Contact = () => {
     return (
         <section className='contact' id="contact" >
-            <p>Contact From</p>
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} md={6}>
+                        <img src={''} alt="Contact Us" />
+                    </Col>
+                    <Col size={12} md={6}>
+                        <h2>Get In Touch</h2>
+                        <form>
+                            <Row>
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="text" value={''} placeholder="First Name" />
+                                </Col>
+
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="text" value={''} placeholder="Last Name" />
+                                </Col>
+
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="email" value={''} placeholder="Email Address" />
+                                </Col>
+
+                                <Col size={12} sm={6} className="px-1">
+                                    <input type="tel" value={''} placeholder="Phone No." />
+                                </Col>
+
+                                <Col size={12} className="px-1">
+                                    <textarea rows="6" value={''} placeholder="Message" ></textarea>
+                                    <button><span>Send</span></button>
+                                </Col>
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     );
 }
