c52f10ebcf9714bd4ac8c9e1d360ba0d00a7dae7
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 16:40:46 +0000

    Applying animate and react-on-screen

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 const Banner = () => {
     const [loopNum, setLoopNum] = useState(1);
@@ -50,12 +52,17 @@ const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Patrick - `}<span className="wrap">{text}</span></h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <button>Let's connect <ArrowRightCircle size={25} /></button>
-                    </Col>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
 
+                                    <span className="tagline">Welcome to my Portfolio</span>
+                                    <h1>{`Hi I'm Patrick - `}<span className="wrap">{text}</span></h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <button>Let's connect <ArrowRightCircle size={25} /></button>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header image" />
                     </Col>
