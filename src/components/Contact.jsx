754016855edc78df52196f4c21c73d3f2a520f64
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 12:57:43 +0000

    Contact importing state and link values from form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import contactImg from '../assets/img/contact-img.svg';
 
 const Contact = () => {
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: ''
+    };
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+
     return (
         <section className='contact' id="contact" >
             <Container>
@@ -14,23 +25,23 @@ const Contact = () => {
                         <form>
                             <Row>
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="text" value={''} placeholder="First Name" />
+                                    <input type="text" value={formDetails.firstName} placeholder="First Name" />
                                 </Col>
 
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="text" value={''} placeholder="Last Name" />
+                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" />
                                 </Col>
 
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="email" value={''} placeholder="Email Address" />
+                                    <input type="email" value={formDetails.email} placeholder="Email Address" />
                                 </Col>
 
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="tel" value={''} placeholder="Phone No." />
+                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." />
                                 </Col>
 
                                 <Col size={12} className="px-1">
-                                    <textarea rows="6" value={''} placeholder="Message" ></textarea>
+                                    <textarea rows="6" value={formDetails.message} placeholder="Message" ></textarea>
                                     <button><span>Send</span></button>
                                 </Col>
                             </Row>
