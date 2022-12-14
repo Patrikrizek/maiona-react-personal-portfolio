881407d3efeb5a804eb40bb11135e0bc58942d6a
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 14:14:52 +0000

    Footer Bootstrap import and content upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>Footer</p>
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} sm={6}>
+                        <img src={''} alt="Logo" />
+                    </Col>
+                    <Col size={12} sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href="#"><img src={''} alt="Icon" /></a>
+                            <a href="#"><img src={''} alt="Icon" /></a>
+                            <a href="#"><img src={''} alt="Icon" /></a>
+                        </div>
+                        <p>Copyright 2022. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     );
 }
