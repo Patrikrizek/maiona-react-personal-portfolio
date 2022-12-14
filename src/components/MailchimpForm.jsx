b96ff840527ed8c39fd1c67e074c406c4a6015bd
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 14:35:44 +0000

    Creating Environment Variables

--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 const MailchimpForm = () => {
-    const SimpleForm = () => <MailchimpSubscribe url={'url'} />
+    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
     return (
         <React.Fragment>
