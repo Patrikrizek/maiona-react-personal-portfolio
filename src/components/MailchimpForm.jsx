5816223d92404cab6f24e4886384701d36d8c41e
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 14:51:54 +0000

    MailchimpForm customized content and Newsletter component created

--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -1,23 +1,21 @@
-import React from "react";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import Newsletter from "./Newsletter";
 
 const MailchimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
     return (
-        <React.Fragment>
+        <>
             <MailchimpSubscribe
-                url={url}
+                url={postUrl}
                 render={({ subscribe, status, message }) => (
-                    <div>
-                        <SimpleForm onSubmitted={formData => subscribe(formData)} />
-                        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-                        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{ __html: message }} />}
-                        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
-                    </div>
+                    <Newsletter
+                        status={status}
+                        message={message}
+                        onValidate={formData => subscribe(formData)} />
                 )}
             />
-        </React.Fragment>
+        </>
     );
 }
 
