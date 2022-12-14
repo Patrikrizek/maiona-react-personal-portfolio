25648424d72399af14fbeb0363e4a01adf4c8e33
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 14:32:30 +0000

    Mailchimp default template upload

--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -1,6 +1,23 @@
+import React from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+
 const MailchimpForm = () => {
+    const SimpleForm = () => <MailchimpSubscribe url={'url'} />
+
     return (
-        <p>Mailchimp form</p>
+        <React.Fragment>
+            <MailchimpSubscribe
+                url={url}
+                render={({ subscribe, status, message }) => (
+                    <div>
+                        <SimpleForm onSubmitted={formData => subscribe(formData)} />
+                        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+                        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{ __html: message }} />}
+                        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+                    </div>
+                )}
+            />
+        </React.Fragment>
     );
 }
 
