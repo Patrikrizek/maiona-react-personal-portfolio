ce447106766d4d5cb539f8420b984dc0b529602a
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 13:05:48 +0000

    Contact handleSubmit async function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,6 +22,29 @@ const Contact = () => {
         })
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setButtonText("Sending...");
+
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(formDetails),
+        });
+
+        setButtonText("Send");
+        let result = await response.json();
+        setFormDetails(formInitialDetails);
+
+        if (result.code == 200) {
+            setStatus({ success: true, message: 'Message sent successfully' });
+        } else {
+            setStatus({ success: false, message: 'Something went wrong, please try again later.' });
+        }
+    };
+
     return (
         <section className='contact' id="contact" >
             <Container>
@@ -31,7 +54,7 @@ const Contact = () => {
                     </Col>
                     <Col size={12} md={6}>
                         <h2>Get In Touch</h2>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <Row>
                                 <Col size={12} sm={6} className="px-1">
                                     <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
@@ -51,7 +74,7 @@ const Contact = () => {
 
                                 <Col size={12} className="px-1">
                                     <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                                    <button><span>{buttonText}</span></button>
+                                    <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
 
                                 {
