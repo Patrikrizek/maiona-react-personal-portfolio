277537bfebbc8d83c260ee7a14e2a2079ddb974e
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-12-14 13:04:25 +0000

    Contact onFormUpdate function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,6 +15,13 @@ const Contact = () => {
     const [buttonText, setButtonText] = useState('Send');
     const [status, setStatus] = useState({});
 
+    const onFormUpdate = (category, value) => {
+        setFormDetails({
+            ...formDetails,
+            [category]: value
+        })
+    };
+
     return (
         <section className='contact' id="contact" >
             <Container>
@@ -27,23 +34,23 @@ const Contact = () => {
                         <form>
                             <Row>
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="text" value={formDetails.firstName} placeholder="First Name" />
+                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                                 </Col>
 
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" />
+                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                                 </Col>
 
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email Address" />
+                                    <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                                 </Col>
 
                                 <Col size={12} sm={6} className="px-1">
-                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." />
+                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
                                 </Col>
 
                                 <Col size={12} className="px-1">
-                                    <textarea rows="6" value={formDetails.message} placeholder="Message" ></textarea>
+                                    <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                                     <button><span>{buttonText}</span></button>
                                 </Col>
 
